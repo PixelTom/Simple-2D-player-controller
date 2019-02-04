ad21e23646c84d0404ad491ccac6e266a3dbc9e5
PixelTom <tom@pixeltom.net>
2019-02-04 21:12:33 +1100

    Tutorial dump

---
+++ Assets/PlayerController2D.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+
+    Animator animator;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+
+    bool isGrounded;
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    Transform groundCheckL;
+    [SerializeField]
+    Transform groundCheckR;
+    [SerializeField]
+    private float runSpeed = 1.5f;
+    [SerializeField]
+    private float jumpSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void FixedUpdate()
+    {
+        string queuedAnim = "Player_Idle";
+        isGrounded = false;
+
+        if(Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")) ||
+            Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground")) ||
+            Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            if(rb2d.velocity.y <= 1)
+            {
+                isGrounded = true;
+            }
+        }
+
+        if(Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(runSpeed, rb2d.velocity.y);
+            queuedAnim = "Player_Run";
+            spriteRenderer.flipX = false;
+        }
+        else if(Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-runSpeed, rb2d.velocity.y);
+            queuedAnim = "Player_Run";
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+
+        if(!isGrounded){
+            queuedAnim = "Player_Jump";
+        }
+
+        if(Input.GetKey("space") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+        }
+
+        animator.Play(queuedAnim);
+    }
+}
